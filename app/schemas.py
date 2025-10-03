5cbaf97047ede03abeba2e1353919e0e456e671a
Joseph Javier Sánchez Acuña <joseph.sanchez@uniminuto.edu.co>
2025-10-02 23:29:35 -0500

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -1,1 +1,32 @@
+from pydantic import BaseModel
 
+# --- Schemas para Inventory ---
+
+class InventoryItemBase(BaseModel):
+    name: str
+    quantity: int
+
+class InventoryItemCreate(InventoryItemBase):
+    pass
+
+class InventoryItem(InventoryItemBase):
+    id: int
+
+    class Config:
+        orm_mode = True # Permite a Pydantic leer datos desde modelos ORM
+
+# --- Schemas para Orders (a ser utilizados en el futuro) ---
+
+class OrderBase(BaseModel):
+    title: str
+    price: float
+
+class OrderCreate(OrderBase):
+    pass
+
+class Order(OrderBase):
+    id: int
+    status: str
+
+    class Config:
+        orm_mode = True
